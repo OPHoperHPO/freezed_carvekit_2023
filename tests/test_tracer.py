e9b04a96d6c75a2bddea87b8c332c07e266c157f
Nikita Selin (Anodev) <farvard34@gmail.com>
2022-09-27 17:55:20 +1000

    Fixed typos in tests

--- tests/test_tracer.py
+++ tests/test_tracer.py
@@ -5,7 +5,8 @@ from PIL import Image
 from carvekit.ml.wrap.tracer_b7 import TracerUniversalB7
 
 def test_init():
-    TracerUniversalB7(input_image_size=[320, 320], load_pretrained=True)
+    TracerUniversalB7(input_image_size=[640, 640], load_pretrained=True)
+    TracerUniversalB7(input_image_size=640, load_pretrained=True)
     TracerUniversalB7(load_pretrained=False)
     TracerUniversalB7(fp16=True)
 
@@ -16,9 +17,10 @@ def test_preprocessing(tracer_model, converted_pil_image, black_image_pil):
     assert isinstance(tracer_model.data_preprocessing(black_image_pil), torch.FloatTensor) is True
 
 
+
 def test_postprocessing(tracer_model, converted_pil_image, black_image_pil):
     tracer_model = tracer_model(False)
-    assert isinstance(tracer_model.data_postprocessing(torch.ones((1, 320, 320), dtype=torch.float64),
+    assert isinstance(tracer_model.data_postprocessing(torch.ones((1, 640, 640), dtype=torch.float64),
                                                       converted_pil_image), Image.Image)
 
 
