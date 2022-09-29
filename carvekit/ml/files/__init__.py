6c87a8f6d55f475b16c73d26d0a1c9edaebcb112
Максим Костюков <enginer385@gmail.com>
2022-09-29 20:02:52 +1100

    Create .cache directory before rename

--- carvekit/ml/files/__init__.py
+++ carvekit/ml/files/__init__.py
@@ -4,6 +4,7 @@ carvekit_dir = Path.home().joinpath('.carvekit')
 
 if carvekit_dir.exists():  # backwards compatibility. Moved from home to cache to reduce clutter in home dir
     print('Moving models from home to .cache (backwards compatibility fix)')
+    Path.home().joinpath('.cache').mkdir(parents=True, exist_ok=True)
     carvekit_dir.rename(Path.home().joinpath('.cache/carvekit'))
 
 carvekit_dir = Path.home().joinpath('.cache/carvekit')
